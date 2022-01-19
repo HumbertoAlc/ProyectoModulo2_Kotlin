900955799d5c6271d1d89113e88f2ef98c3d3d60
humberto <galactico10@live.com>
2022-01-19 00:47:26 -0600

    se sube el prooyecto a git

---
+++ app/src/main/java/com/example/proyectomodulo2/DetalleDeLibros.kt
@@ -0,0 +1,11 @@
+package com.example.proyectomodulo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalleDeLibros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_de_libros)
+    }
+}
---
+++ app/src/main/java/com/example/proyectomodulo2/ListaDeLibros.kt
@@ -0,0 +1,11 @@
+package com.example.proyectomodulo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaDeLibros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_de_libros)
+    }
+}
